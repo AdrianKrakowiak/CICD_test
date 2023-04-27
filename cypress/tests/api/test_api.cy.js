95c6348b92f33c5dbb1a56720f0022ccf1c662fb
Adrian K <adrian-krako@wp.pl>
2023-04-27 23:07:16 +0200

    dopracowanie_2

--- cypress/tests/api/test_api.cy.js
+++ cypress/tests/api/test_api.cy.js
@@ -81,20 +81,6 @@ describe('POST request with response status check', () => {
       })
   })
 
-  describe('GET request with negative scenario when url is wrong', () => {
-    const request = {
-      method: 'GET',
-      url: 'https://httpbin.org/ge',
-      failOnStatusCode: false
-    };
-  
-    it('response code should be 405', () => {
-      cy.request(request).then(response => {
-        assert.equal(405, response.status);
-      })
-    })
-  })
-
   describe('GET request with negative scenario when method used is wrong', () => {
     const request = {
       method: 'GET',
